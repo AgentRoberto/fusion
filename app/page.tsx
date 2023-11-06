1f023003c1bd46ecb65b44e5f8f2697d1e7f19f2
agentroberto <sherwanirobert@gmail.com>
2023-11-06 17:04:42 -0500

    Feat: Posts pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import Link from 'next/link'
+import Posts from './components/Posts'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
           I'm <span className='font-bold'>Rob</span>
         </span>
       </p>
+      <Posts />
       <Link href="/about">Go to About Page</Link>
       <Link href="/users">Go to Users Page</Link>
 
